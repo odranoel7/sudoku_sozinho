1c30e748951754d22a7f003fe8f9da9cba178144
Leonardo Déo <leonardodeo01@gmail.com>
2020-06-08 22:30:15 -0300

    1,2,4

---
+++ 4.py
@@ -0,0 +1,21 @@
+def backtracking(i, vetor):
+    if (i == 81):
+        imprime(vetor)
+        return True
+    elif (vetor[i] != 0):
+        return backtracking(i+1, vetor)
+    for j in range(0,9):
+        if nao_ha_violacao(j, i, vetor):
+            vetor[i] = j
+            if backtracking(i+1, vetor):
+                imprime(vetor)
+                return True
+            vetor[i] = 0
+    return False
+
+
+def nao_ha_violacao(x, i, vetor):
+    pass
+
+def imprime(vetor):
+    pass
